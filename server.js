37fd29840ea39f69af8ab666eed514796c1d98de
Haekalsss <kallsss145@gmail.com>
2025-07-27 20:53:49 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const { Server } = require('socket.io');
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+});
+
+// Endpoint untuk trigger event dari API donasi (Vercel)
+app.post('/notify', (req, res) => {
+  io.emit('new-donation', req.body);
+  res.json({ success: true });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Socket.io server running on port', PORT);
+});
