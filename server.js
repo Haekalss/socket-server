d3b08438d7a47d91c7b83414e3bb4bb3faafddc9
Haekalsss <kallsss145@gmail.com>
2025-07-27 21:08:48 +0700

    ganti cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: { origin: 'http://localhost:3000' }
 });
 
 io.on('connection', (socket) => {
